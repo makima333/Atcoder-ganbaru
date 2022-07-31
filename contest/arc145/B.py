894a43e577cb1a09a7a67e084a99cd343d9cb7ce
makima333 <you@example.com>
2022-07-31 21:16:48 +0900

    arc145 Bの回答を追加

--- contest/arc145/B.py
+++ contest/arc145/B.py
@@ -2,10 +2,10 @@ import io
 import sys
 
 _INPUT = """\
-50 4 8
+4 2 1
+
 """
 sys.stdin = io.StringIO(_INPUT)
-# 不正解でした
 #---------------------------------
 tmp = input()
 N = int(tmp.split(" ")[0])
@@ -13,21 +13,20 @@ A = int(tmp.split(" ")[1])
 B = int(tmp.split(" ")[2])
 
 a_win = 0
-for i in range(N):
-    n = i + 1
-    while True:
-        # aliceのたーん
-        if n >= A:
-            nko = n // A
-            n -= A*nko
-        else:
-            break
+laund = N-(A-1)
 
-        # bobのたーん
-        if n >= B:
-            nko = n // B
-            n -= B*nko
-        else:
-            a_win += 1
-            break
-print(a_win)
+if A <= B:
+    a_win = laund
+elif laund != 0:
+    # Aの勝利条件
+    # n mod A < B
+    # 10 4 3 のとき
+    # n = 1,2,3,4,5,6,7,8,9,10
+    # n mod A = 1,2,3,0,1,2,3,0,1,2
+    # n mod A < B = 1,2,0,1,2,0,1,2
+    # mod の周期数 0,1,2,3部分のうち勝てるのはB個
+
+    a_win = (laund//A) * B + min(laund%A, B)
+
+
+print(max(a_win,0))
